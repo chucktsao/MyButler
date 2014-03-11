90734ed10da473e4f6819276815dafbfe2f66349
chucktsao@126.com <chucktsao@126.com>
2014-03-11 17:02:36 +0800

    -modify

--- android/MyButler/src/com/ct/butler/MainActivity.java
+++ android/MyButler/src/com/ct/butler/MainActivity.java
@@ -1,7 +1,16 @@
 package com.ct.butler;
 
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -14,6 +23,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		getSingInfo();
 	}
 
 	@Override
@@ -24,9 +34,31 @@ public class MainActivity extends Activity {
 	}
 	
 	
-	public void test(){
-		BaseAdapter a = null;
-		a.notifyDataSetChanged();
+
+	public void getSingInfo() {
+		try {
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(
+					"com.ct.butler", PackageManager.GET_SIGNATURES);
+			Signature[] signs = packageInfo.signatures;
+			Signature sign = signs[0];
+			parseSignature(sign.toByteArray());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void parseSignature(byte[] signature) {
+		try {
+			CertificateFactory certFactory = CertificateFactory
+					.getInstance("X.509");
+			X509Certificate cert = (X509Certificate) certFactory
+					.generateCertificate(new ByteArrayInputStream(signature));
+			String pubKey = cert.getPublicKey().toString();
+			String signNumber = cert.getSerialNumber().toString();
+			Log.d("test", "pubKey:"+pubKey+" signNumber:"+signNumber);
+		} catch (CertificateException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
